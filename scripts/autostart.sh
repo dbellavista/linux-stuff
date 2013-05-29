f22a96dc101be2ad0ae275934da40320bb1f822e
Daniele Bellavista <daniele.bellavista@studio.unibo.it>
2013-05-29 09:56:25 +0200

    Nm-applet to wicd

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 #sleep 3
 #sleep 7
-bash ~/linux/scripts/run_once.sh "nm-applet" "nm-applet"
+#bash ~/linux/scripts/run_once.sh "nm-applet" "nm-applet"
+bash ~/linux/scripts/run_once.sh "wicd-client.py" "wicd-gtk"
 bash ~/linux/scripts/run_once.sh "dropbox" "/home/daniele/.dropbox-dist/dropboxd"
