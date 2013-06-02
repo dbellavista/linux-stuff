ba5cee66152cd1eaee71625125cba4590b4f25c4
Daniele Bellavista <daniele.bellavista@studio.unibo.it>
2013-06-02 10:38:45 +0200

    Wicd starts without window

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -2,5 +2,5 @@
 #sleep 3
 #sleep 7
 #bash ~/linux/scripts/run_once.sh "nm-applet" "nm-applet"
-bash ~/linux/scripts/run_once.sh "wicd-client.py" "wicd-gtk"
+bash ~/linux/scripts/run_once.sh "wicd-client.py" "wicd-gtk -t"
 bash ~/linux/scripts/run_once.sh "dropbox" "/home/daniele/.dropbox-dist/dropboxd"
