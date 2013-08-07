ebaf1f86f9b66e8d04226d776f3a14d4f840b65e
Daniele Bellavista <daniele.bellavista@studio.unibo.it>
2013-08-07 10:01:58 +0200

    Multiuser run_once

--- scripts/run_once.sh
+++ scripts/run_once.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Run program unless it's already running.
 
-if ! ps ax | grep $1 | grep -v grep | grep -v $0  > /dev/null
+USER=`whoami`
+
+if ! ps aux | grep $1 | grep $USER | grep -v grep | grep -v $0  > /dev/null
 then
 	${@:2} &
 fi
