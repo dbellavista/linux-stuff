3e201a981ffbe35ccab0a3545ba9338adb52951e
Daniele Bellavista <daniele.bellavista@gmail.com>
2013-08-27 10:22:19 +0200

    Fix for new ps usage

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -2,6 +2,6 @@
 #sleep 3
 #sleep 7
 #bash ~/linux/scripts/run_once.sh "nm-applet" "nm-applet"
-bash ~/linux/scripts/run_once.sh "wicd-client.py" "wicd-gtk -t"
+bash ~/linux/scripts/run_once.sh "wicd-client" "wicd-gtk -t"
 #bash ~/linux/scripts/run_once.sh "dropbox" "$HOME/linux/scripts/start_dropbox.sh"
 #bash ~/linux/scripts/run_once.sh "conky" "conky -c $HOME/.conky/conkyrc"
