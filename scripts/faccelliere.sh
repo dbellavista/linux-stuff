8a9dcf7a9b27886e05f6a3b298c0ef4c1af054a4
Daniele Bellavista <daniele.bellavista@studio.unibo.it>
2013-10-27 09:11:23 +0100

    Script for adjusting facl settings

---
+++ scripts/faccelliere.sh
@@ -0,0 +1,25 @@
+if [[ $# -ne 1 ]] ; then
+  echo "Usage $0 <file_or_path>"
+  exit 1
+fi
+
+FIND_DIR=$(pwd)/$1
+
+if [[ ! -e $FIND_DIR ]] ; then
+  echo "Sorry $FIND_DIR doesn't exists!"
+  exit 1
+fi;
+
+echo "Working on: $FIND_DIR"
+
+echo "Setting permits to listable directories"
+find $1 -type d -executable -exec setfacl -d -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec setfacl -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec chmod 770 {} \;
+
+echo "Setting permits to unlistable directories"
+find $1 -type d ! -executable -exec setfacl -d -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec setfacl -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec chmod 660 {} \;
+
+echo "Setting permits to executable files"
+find $1 -type f -executable -exec setfacl -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec chmod 770 {} \;
+
+echo "Setting permits to non executable files"
+find $1 -type f ! -executable -exec setfacl -m u:daniele:rw,u:dbellavista:rw {} \; -exec chmod 660 {} \;
