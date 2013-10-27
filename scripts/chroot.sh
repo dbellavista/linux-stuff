920cf5ace3aa57e79f4d619d29e1edb345cfbf35
Daniele Bellavista <daniele.bellavista@studio.unibo.it>
2013-10-27 09:29:39 +0100

    Script for prepare and do a chroot

---
+++ scripts/chroot.sh
@@ -0,0 +1,41 @@
+#/bin/bash
+
+if [[ $# -ne 1 ]] ; then
+  echo "Usage: $0 <directory>"
+  exit 1
+fi;
+
+if [[ $EUID -ne 0 ]]; then
+  echo "You must be a root user" 2>&1
+  exit 1
+fi
+
+if [[ "$1" = /* ]]
+then
+  DIR=$1
+else
+  DIR=$(pwd)/$1
+fi
+
+if [[\
+  (! -e $DIR) || \
+  (! -e $DIR/dev) || \
+  (! -e $DIR/dev/pts) || \
+  (! -e $DIR/proc) || \
+  (! -e $DIR/sys) || \
+  (! -e $DIR/tmp) \
+  ]] ; then
+  echo "Sorry $DIR doesn't exists or isn't a valid location for chroot!"
+  echo "Have you mounted the filesystem in $DIR?"
+  exit 1
+fi;
+
+mount -o bind /dev $1/dev
+mount -o bind /dev/pts $1/dev/pts
+mount -t proc none $1/proc
+mount -o bind /sys $1/sys
+mount -o bind /tmp $1/tmp
+
+cd $1
+
+chroot . /bin/bash
