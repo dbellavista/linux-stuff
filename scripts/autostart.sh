b6920535dfd5417c3cdff4e9cfb60fa04f9bbaf8
Daniele Bellavista <daniele.bellavista@studio.unibo.it>
2013-11-18 22:19:14 +0100

    A commented blueman applet

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -5,4 +5,5 @@
 bash ~/linux/scripts/run_once.sh "wicd-client" "wicd-gtk -t"
 bash ~/linux/scripts/run_once.sh "dropbox" "$HOME/linux/scripts/start_dropbox.sh"
 bash ~/linux/scripts/run_once.sh "redshift" "redshift"
+#bash ~/linux/scripts/run_once.sh "blueman-applet" "blueman-applet"
 #bash ~/linux/scripts/run_once.sh "conky" "conky -c $HOME/.conky/conkyrc"
