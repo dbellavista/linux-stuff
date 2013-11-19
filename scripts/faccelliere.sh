caebd285d32158557f7f1012d2c8cd69d3b4931b
Daniele Bellavista <Daniele_Bellavista@iprel.it>
2013-11-19 09:15:53 +0100

    Dealing with spaced directories

--- scripts/faccelliere.sh
+++ scripts/faccelliere.sh
@@ -5,9 +5,9 @@ fi
 
 if [[ "$1" = /* ]]
 then
-  FIND_DIR=$1
+  FIND_DIR="$1"
 else
-  FIND_DIR=$(pwd)/$1
+  FIND_DIR=$(pwd)/"$1"
 fi
 
 if [[ ! -e $FIND_DIR ]] ; then
@@ -18,13 +18,13 @@ fi;
 echo "Working on: $FIND_DIR"
 
 echo "Setting permits to listable directories"
-find $1 -type d -executable -exec setfacl -d -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec setfacl -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec chmod 770 {} \;
+find "$FIND_DIR" -type d -executable -exec setfacl -d -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec setfacl -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec chmod 770 {} \;
 
 echo "Setting permits to unlistable directories"
-find $1 -type d ! -executable -exec setfacl -d -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec setfacl -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec chmod 660 {} \;
+find "$FIND_DIR" -type d ! -executable -exec setfacl -d -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec setfacl -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec chmod 660 {} \;
 
 echo "Setting permits to executable files"
-find $1 -type f -executable -exec setfacl -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec chmod 770 {} \;
+find "$FIND_DIR" -type f -executable -exec setfacl -m u:daniele:rwx,u:dbellavista:rwx {} \; -exec chmod 770 {} \;
 
 echo "Setting permits to non executable files"
-find $1 -type f ! -executable -exec setfacl -m u:daniele:rw,u:dbellavista:rw {} \; -exec chmod 660 {} \;
+find "$FIND_DIR" -type f ! -executable -exec setfacl -m u:daniele:rw,u:dbellavista:rw {} \; -exec chmod 660 {} \;
