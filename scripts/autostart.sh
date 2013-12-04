22719bfad64c6c4bf13179e14e780f1a07f618d7
Daniele Bellavista <daniele.bellavista@studio.unibo.it>
2013-12-04 23:52:37 +0100

    Unclutter :)

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -5,5 +5,6 @@
 bash ~/linux/scripts/run_once.sh "wicd-client" "wicd-gtk -t"
 bash ~/linux/scripts/run_once.sh "dropbox" "$HOME/linux/scripts/start_dropbox.sh"
 bash ~/linux/scripts/run_once.sh "redshift" "redshift"
+bash ~/linux/scripts/run_once.sh "unclutter" "unclutter -jitter 50 -idle 2"
 #bash ~/linux/scripts/run_once.sh "blueman-applet" "blueman-applet"
 #bash ~/linux/scripts/run_once.sh "conky" "conky -c $HOME/.conky/conkyrc"
