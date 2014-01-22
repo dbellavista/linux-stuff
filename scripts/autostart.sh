6f581daffabf5f0dd949ada5d58372823a90d4e8
Daniele Bellavista <Daniele_Bellavista@iprel.it>
2014-01-22 07:37:25 +0100

    Using nm-applet

--- scripts/autostart.sh
+++ scripts/autostart.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 #sleep 3
 #sleep 7
-#bash ~/linux/scripts/run_once.sh "nm-applet" "nm-applet"
-bash ~/linux/scripts/run_once.sh "wicd-client" "wicd-gtk -t"
+bash ~/linux/scripts/run_once.sh "nm-applet" "nm-applet"
+#bash ~/linux/scripts/run_once.sh "wicd-client" "wicd-gtk -t"
 bash ~/linux/scripts/run_once.sh "dropbox" "$HOME/linux/scripts/start_dropbox.sh"
 bash ~/linux/scripts/run_once.sh "redshift" "redshift"
-bash ~/linux/scripts/run_once.sh "unclutter" "unclutter -jitter 50 -idle 2"
+bash ~/linux/scripts/run_once.sh "unclutter" "unclutter -jitter 25 -idle 2"
 #bash ~/linux/scripts/run_once.sh "blueman-applet" "blueman-applet"
 #bash ~/linux/scripts/run_once.sh "conky" "conky -c $HOME/.conky/conkyrc"
