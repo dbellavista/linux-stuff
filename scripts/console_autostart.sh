727ff7dc47eb642930ec5027a159b5d18505cab0
Daniele Bellavista <daniele.bellavista@gmail.com>
2014-05-17 17:55:16 +0200

    Opz

--- scripts/console_autostart.sh
+++ scripts/console_autostart.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export $PATH="$PATH $STUFF/bin"
+export PATH="$PATH $STUFF/bin"
 
 bash ~/linux/scripts/run_once.sh "dropbox" "$HOME/linux/scripts/start_dropbox.sh"
 bash ~/linux/scripts/run_once.sh "redshift" "redshift"
