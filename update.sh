0827e0b326840dc8e3502eb296aa13546a365e53
Daniele Bellavista <daniele.bellavista@gmail.com>
2014-10-09 09:28:44 +0200

    Update script

---
+++ update.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echo " [>] Updating submodules"
+git submodule foreach git pull origin master:master
+git submodule foreach git submodule update --force --init --recursive
+echo " [>] Building YouCompleteMe"
+cd ./vim/vim/bundle/YouCompleteMe/
+./install.sh --clang-completer --system-libclang
