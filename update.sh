f212d1b596007cbfb2bd131f5831f6a7369d86e4
Daniele Bellavista <daniele.bellavista@gmail.com>
2014-12-05 09:17:52 +0100

    Using STUFF var

--- update.sh
+++ update.sh
@@ -4,7 +4,8 @@ echo " [>] Updating submodules"
 git submodule foreach git pull origin master:master
 git submodule foreach git submodule update --force --init --recursive
 echo " [>] Building YouCompleteMe"
-cd ./vim/vim/bundle/YouCompleteMe/
+cd $STUFF/vim/vim/bundle/YouCompleteMe/
 ./install.sh --clang-completer --system-libclang
+echo " [>] Updating tern"
 cd $STUFF/vim/vim/bundle/tern_for_vim/
 npm install
